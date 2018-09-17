c8d1730d3618daba3ab328dbb39c6a53064fec4b
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2018-09-17 14:12:38 -0300

    GnomeSort ok

---
+++ R04-01/R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
@@ -0,0 +1,33 @@
+package sorting.variationsOfBubblesort;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * The implementation of the algorithm must be in-place!
+ */
+
+public class GnomeSort<T extends Comparable<T>> extends AbstractSorting<T> {
+	
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		// inicializa o pivot
+		int posicao = leftIndex + 1; 
+		// enquanto nao ultrapassa o ultimo elemento
+		while(posicao <= rightIndex) {
+			// Se estiver na ordem correta, incrementa o pivot
+			if(array[posicao].compareTo(array[posicao - 1]) >= 0) {
+				posicao ++;
+			// Se nao, faz a troca das posicoes
+			}else {
+				Util.swap(array, posicao, posicao - 1);
+				// verifica se tem anterior
+				if(posicao > leftIndex + 1) {
+					posicao --;
+				// se nao, prossegue.	
+				}else {
+					posicao ++;
+				}
+			}
+		}
+	}
+}
