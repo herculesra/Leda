c0adb22370c508b6509d8955898df61f961bd0a6
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2018-09-27 11:49:02 -0300

    Stack Ok

--- R05-01/R05-01-Rot-Pilha-FilaCircular-environment/src/main/java/adt/stack/StackImpl.java
+++ R05-01/R05-01-Rot-Pilha-FilaCircular-environment/src/main/java/adt/stack/StackImpl.java
@@ -22,26 +22,31 @@ public class StackImpl<T> implements Stack<T> {
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return top == -1;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return top == array.length -1;
+		
 	}
 
 	@Override
 	public void push(T element) throws StackOverflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		try {
+			array[++ this.top] = element;
+		}catch(Exception e) {
+			throw new StackOverflowException();
+		}
 	}
 
 	@Override
 	public T pop() throws StackUnderflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(isEmpty()) {
+			throw new StackUnderflowException();
+		}else {
+			return array[-- this.top];
+		}
 	}
 
 }
