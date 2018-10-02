dab065e0e708be787b5d608fc0194d0ba752a73c
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2018-10-02 10:04:27 -0300

    Single Linked List

--- R06-01/R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
+++ R06-01/R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -2,73 +2,92 @@ package adt.linkedList;
 
 public class SingleLinkedListImpl<T> implements LinkedList<T> {
 
-	protected SingleLinkedListNode<T> head;
+   protected SingleLinkedListNode<T> head;
 
-	public SingleLinkedListImpl() {
-		this.head = new SingleLinkedListNode<T>();
-	}
+   public SingleLinkedListImpl() {
+      this.head = new SingleLinkedListNode<T>();
+   }
 
-	@Override
-	public boolean isEmpty() {
-		return this.head.isNIL();
-	}
+   @Override
+   public boolean isEmpty() {
+      return this.head.isNIL();
+   }
 
-	@Override
-	public int size() {
-		int size = 0;
-		SingleLinkedListNode<T> aux = head;
-		while(!aux.isNIL()) {
-			size ++;
-			aux = aux.getNext();
-		}
-		return size;
-	}
-	
-	@Override
-	public T search(T element) {
-		T result = null;
-		
-		SingleLinkedListNode<T> aux = this.head;
-		while(result != element && !aux.getNext().isNIL()) {
-			result = aux.getNext().getData() ;				
-			aux = aux.getNext();
-		}
-		return result;
-	}
+   @Override
+   public int size() {
+      int size = 0;
+      SingleLinkedListNode<T> aux = head;
+      while (!aux.isNIL()) {
+         size++;
+         aux = aux.getNext();
+      }
+      return size;
+   }
 
-	@Override
-	public void insert(T element) {
-		if(isEmpty()) {
-			this.head.setData(element);
-			this.head.setNext(new SingleLinkedListNode<T>());
-		}else {
-			SingleLinkedListNode<T> aux = head;
-			while(!aux.getNext().isNIL()) {
-				aux = aux.getNext();
-			}
-			SingleLinkedListNode<T> newNode = new SingleLinkedListNode<>(element, new SingleLinkedListNode<>());
-			aux.setNext(newNode);
-		}
-	}
+   @Override
+   public T search(T element) {
+      T result = null;
+      SingleLinkedListNode<T> aux = this.head;
+      while (!aux.isNIL()) {
+         if (aux.getData().equals(element)) {
+            result = aux.getData();
+         }
+         aux = aux.getNext();
+      }
+      return result;
+   }
 
-	@Override
-	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
-	}
+   @Override
+   public void insert(T element) {
+      if (isEmpty()) {
+         this.head.setData(element);
+         this.head.setNext(new SingleLinkedListNode<T>());
+      } else {
+         SingleLinkedListNode<T> aux = head;
+         while (!aux.getNext().isNIL()) {
+            aux = aux.getNext();
+         }
+         SingleLinkedListNode<T> newNode = new SingleLinkedListNode<>(element, new SingleLinkedListNode<>());
+         aux.setNext(newNode);
+      }
+   }
 
-	@Override
-	public T[] toArray() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
-	}
+   @Override
+   public void remove(T element) {
+      if (head.getData().equals(element)) {
+         setHead(head.next);
+      } else {
+         SingleLinkedListNode<T> aux = head;
+         SingleLinkedListNode<T> previous = aux;
+         while (!aux.isNIL() && !aux.getData().equals(element)) {
+            previous = aux;
+            aux = aux.getNext();
+         }
+         if (!aux.isNIL()) {
+            previous.next = aux.next;
+         }
+      }
+   }
 
-	public SingleLinkedListNode<T> getHead() {
-		return head;
-	}
+   @Override
+   public T[] toArray() {
+      SingleLinkedListNode<T> aux = head;
+      T[] result = (T[]) new Object[size()];
+      int index = 0;
+      while (!aux.isNIL()) {
+         result[index] = aux.data;
+         aux = aux.getNext();
+         index++;
+      }
+      return result;
+   }
 
-	public void setHead(SingleLinkedListNode<T> head) {
-		this.head = head;
-	}
+   public SingleLinkedListNode<T> getHead() {
+      return head;
+   }
+
+   public void setHead(SingleLinkedListNode<T> head) {
+      this.head = head;
+   }
 
 }
