42d71b2b989da660c7d37731f58205bffcbde123
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2018-10-19 18:05:41 -0300

    QuadraticProbing ok

--- R09-01/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ R09-01/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -1,6 +1,7 @@
 package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
+import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
 
 public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
@@ -15,25 +16,68 @@ public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(!isFull()) {
+			if(element != null && search(element) == null) {
+				 boolean collision = true;
+		               int i = 0;
+		               while (collision) {
+		                  int index = ((HashFunctionOpenAddress<T>) getHashFunction()).hash(element, i);
+		                  if (table[index] == null || table[index].equals(super.deletedElement)) {
+		                     table[index] = element;
+		                     super.elements++;
+		                     collision = false;
+		                  } else {
+		                     super.COLLISIONS++;
+		                     i++;
+		                  }
+		               }       
+			}
+		}else {
+			throw new HashtableOverflowException();
+		}
 	}
-
 	@Override
-	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+	   public void remove(T element) {
+	      if (element != null && !isEmpty()) {
+	         int index = indexOf(element);
+	         if (index != -1) {
+	            table[index] = deletedElement;
+	            super.elements--;
+	         }
+	      }
+	   }
 
-	@Override
-	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+	   @SuppressWarnings("unchecked")
+	   @Override
+	   public T search(T element) {
+	      T result = null;
+	      if (element != null && !isEmpty()) {
+	         int index = indexOf(element);
+	         if (index != -1) {
+	            result = (T) table[index];
+	         }
+	      }
+	      return result;
+	   }
 
-	@Override
-	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+	   @Override
+	   public int indexOf(T element) {
+	      int result = -1;
+	      if (element != null && !isEmpty()) {
+	         boolean achou = false;
+	         int i = 0;
+	         int index = ((HashFunctionOpenAddress<T>) getHashFunction()).hash(element, i);
+	         while (!achou && table[index] != null && i < table.length) {
+	            if (table[index].equals(element)) {
+	               result = index;
+	               achou = true;
+	            } else {
+	               i++;
+	            }
+
+	            index = ((HashFunctionOpenAddress<T>) getHashFunction()).hash(element, i);
+	         }
+	      }
+	      return result;
+	   }
 }
